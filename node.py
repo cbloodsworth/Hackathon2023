e8484c9801da150ebb7aab26bcdea323ad623036
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-01-28 12:36:27 -0500

    Added 3 base classes grid, node, player

---
+++ node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self):
+        self.biome = "Plains"
+        self.enemies = {}
+        self.items = {}
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, position):
+        self.backpack = {}
+        self.stats = {"health_points": 10, "move_speed": 1}
+        self.position = position
