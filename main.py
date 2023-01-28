2b7b2b8e5ea2cda9de377e4ed14a6f89f35606eb
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-01-28 18:03:48 -0500

    Player centered on movement

--- main.py
+++ main.py
@@ -26,13 +26,12 @@ start_game = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 200), 
 # In game GUI
 game_gui = pygame_gui.UIManager(screen_size)
 
-
 game_running = True
 game_begin = False
 clock = pygame.time.Clock()
 grid = board.Grid()
 grid.generate_grid(grid_width, grid_height)
-player = plr.Player([2, 2])
+player = plr.Player([0, 0])
 while game_running:
     # Frame rate
     time_delta = clock.tick(60) / 1000.0
@@ -52,27 +51,29 @@ while game_running:
         # Player movement
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            player.move([0, -0.01])
+            player.move([0, 1])
         if keys[pygame.K_s]:
-            player.move([0, 0.01])
+            player.move([0, -1])
         if keys[pygame.K_a]:
-            player.move([-0.01, 0])
+            player.move([1, 0])
         if keys[pygame.K_d]:
-            player.move([0.01, 0])
+            player.move([-1, 0])
 
-        # Set tile colors
+        # Get player position
+        plr_pos = player.position
+
+        # Set tile position and colors
         for x in range(grid_width):
             for y in range(grid_height):
                 color = worldgen.biomeMap[grid.nodes[x][y].terrain.biome]
                 pygame.draw.rect(screen, color,
-                                 pygame.Rect(x * block_size, y * block_size, block_size, block_size))
+                                 pygame.Rect(((x - grid_width // 2) * block_size)
+                                             + plr_pos[0], ((y - grid_height // 2) * block_size) - 250 +
+                                             plr_pos[1], block_size, block_size))
 
         # Player draw
-        player_x = player.position[0]
-        player_y = player.position[1]
         pygame.draw.rect(screen, pygame.Color(255, 0, 0),
-                         pygame.Rect((player_x + 0.25) * block_size, (player_y + 0.25) * block_size, block_size / 2,
-                                     block_size / 2))
+                         pygame.Rect(250 - block_size / 4, 250 - block_size / 4, block_size / 2, block_size / 2))
 
     # GUI
     if game_begin:
