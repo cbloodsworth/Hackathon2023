040702d3621fca724ff1cfa7fe3a8ab091ed3fc2
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-01-28 19:41:36 -0500

    Moved backpack button

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 # Board parameters
 
 
-block_size = 5  # Size of side of the block
+block_size = 25  # Size of side of the block
 
 grid_height, grid_width = 100, 100
 screen_size = min(grid_height * block_size, 1000), min(grid_width * block_size, 650)  # Screen size in pixels
@@ -23,15 +23,19 @@ pygame.display.set_caption("Exploring The Unknown")
 main_background = pygame.Surface(screen_size)
 main_background.fill(pygame.Color('#000000'))
 main_gui = pygame_gui.UIManager(screen_size)
-start_game_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 200), (200, 100)),
-                                              text='Begin Your Journey',
-                                              manager=main_gui)
+start_game_btn = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((screen_center[0] - 100, screen_center[1] - 50), (200, 100)),
+    text='Begin Your Journey',
+    manager=main_gui)
 
 # In game GUI
 game_gui = pygame_gui.UIManager(screen_size, 'theme.json')
-start_game = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 50), (75, 75)),
-                                          text='Backpack',
-                                          manager=game_gui)
+backpack_background = pygame.Surface(screen_size)
+backpack_background.fill(pygame.Color('#000000'))
+open_backpack_btn = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect(screen_size[0] * 0.85, screen_size[1] * 0.825, 100, 75),
+    text='Backpack',
+    manager=game_gui)
 
 game_running = True
 game_begin = False
@@ -39,6 +43,10 @@ clock = pygame.time.Clock()
 grid = board.Grid()
 grid.generate_grid(grid_width, grid_height)
 player = plr.Player([0, 0])
+
+# In game bools
+backpack_open = False
+
 while game_running:
     # Frame rate
     time_delta = clock.tick(60) / 1000.0
@@ -56,6 +64,8 @@ while game_running:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == start_game_btn:
                 game_begin = True
+            if event.ui_element == open_backpack_btn:
+                backpack_open = not backpack_open
     if game_begin:
         # Player movement
         keys = pygame.key.get_pressed()
@@ -77,14 +87,16 @@ while game_running:
                 pygame.draw.rect(screen, col,
                                  pygame.Rect(center_x + plr_x, + center_y + plr_y, block_size, block_size))
 
-
         # Player draw
         pygame.draw.rect(screen, pygame.Color(255, 0, 0),
-                         pygame.Rect(screen_center[0] - block_size / 4, screen_center[1] - block_size / 4, block_size / 2, block_size / 2))
+                         pygame.Rect(screen_center[0] - block_size / 4, screen_center[1] - block_size / 4,
+                                     block_size / 2, block_size / 2))
 
     # GUI
     if game_begin:
         game_gui.update(time_delta)
+        if backpack_open:
+            pass
         game_gui.draw_ui(screen)
     else:
         main_gui.update(time_delta)
