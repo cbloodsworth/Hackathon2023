ad86fd7930ad63e8c7671425433eeb89cbf9ccc3
jonathan1q2w3e4r <jonathanlevy2002@gmail.com>
2023-01-31 06:17:32 -0500

    Refactored the GUI system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import better_gui as ui
+import gui as ui
 import worldgen
 import grid as board
 import player as plr
@@ -7,87 +7,29 @@ from worldgen import *
 import random
 from math import floor
 from grid import *
-random.seed()
+
+# Game needs to start otherwise Font cannot load
 pygame.init()
-# Font
-game_font = pygame.font.SysFont("monospace", 20)
+from gui import gui_manager
 
-game_font_smaller = pygame.font.SysFont("monospace", 16)
-# GUI
+random.seed()
 
-# Main screen UI
-main_screen_ui = ui.UI()  # Initialize UI for main screen
-
-# Start game button
-start_btn = ui.Textbox(game_font)
-start_btn.pos = screen_center
-start_btn.size = [300, 100]
-start_btn.text = "Begin Your Journey"
-start_btn.color = pygame.Color(0, 255, 0, 255)
-main_screen_ui.buttons["Start Button"] = start_btn
-
-# In game screen UI
-game_screen_ui = ui.UI()
-
-# Backpack button
-open_backpack_btn = ui.Textbox(game_font)
-open_backpack_btn.pos = [screen_size[0] * 0.85, screen_size[1] * 0.9]
-open_backpack_btn.size = [200, 90]
-open_backpack_btn.text = "Open Backpack"
-open_backpack_btn.color = pygame.Color(0, 255, 0, 255)
-game_screen_ui.buttons["Open Backpack"] = open_backpack_btn
-
-# Notification text box
-notification = ui.Textbox(game_font_smaller)
-notification.justify = "top"
-notification.pos = [screen_size[0] * 0.85, screen_size[1] * 0.15]
-notification.size = [200, 90]
-notification.color = pygame.Color(255, 255, 255, 180)
-game_screen_ui.objects["Notification"] = notification
-
-# Craft boat button
-craft_button = ui.Textbox(game_font_smaller)
-craft_button.justify = "top"
-craft_button.pos = [screen_size[0] * 0.85, screen_size[1] * 0.45]
-craft_button.size = [200, 90]
-craft_button.color = pygame.Color(255, 255, 255, 180)
-craft_button.text = "5 ??? to craft a boat"
-game_screen_ui.buttons["Craft"] = craft_button
-# Backpack UI
-backpack_ui = ui.UI()
-
-inventory_box = ui.Box()
-inventory_box.size = [screen_size[0] * 0.75, screen_size[1] * 0.75]
-inventory_box.pos = [screen_center[0], screen_center[1] * 0.85]
-inventory_box.color = pygame.Color(0, 0, 0, 150)
-backpack_ui.objects["Inventory Box"] = inventory_box
-
-NUM_ITEMS = 36
-
-for i in range(NUM_ITEMS):
-    inventory_cell = ui.Textbox(game_font_smaller)
-    cell_x = inventory_box.pos[0] + inventory_box.size[0] // 9 * (i % 9) - screen_size[0] * 0.75 // 2 + inventory_box.size[0] * 0.045 + (inventory_box.size[0] * .19 / 18)
-    cell_y = inventory_box.pos[1] + inventory_box.size[1] // 4 * (i // 9) - screen_size[1] * 0.75 // 2 + inventory_box.size[0] * 0.045 + (inventory_box.size[1] * .06)
-    inventory_cell.size = [inventory_box.size[0] * 0.09, inventory_box.size[0] * 0.09]
-    inventory_cell.pos = [cell_x, cell_y]
-    inventory_cell.color = pygame.Color(255, 255, 255, 100)
-    backpack_ui.objects["Inventory Cell " + str(i+1)] = inventory_cell
-
-# List of UIs
-ui_list = [main_screen_ui, game_screen_ui, backpack_ui]
 # Screen initialization
-screen = pygame.display.set_mode(screen_size)
+window = pygame.display.set_mode(screen_size)
+screen = pygame.Surface(screen_size, pygame.SRCALPHA)
+
 pygame.display.set_caption("Swamp Investigator")
 
 # Grid, player, and clock initialization
 clock = pygame.time.Clock()
 world_grid = board.Grid()
 world_grid.generate_grid(grid_width, grid_height)
-rand_x = random.randint(0, grid_width-1)
-rand_y = random.randint(0, grid_height-1)
+rand_x = random.randint(0, grid_width - 1)
+rand_y = random.randint(0, grid_height - 1)
+
 while world_grid.nodes[rand_x][rand_y].biome != Biome.DEEP_OCEAN:
-    rand_x = random.randint(0, grid_width-1)
-    rand_y = random.randint(0, grid_height-1)
+    rand_x = random.randint(0, grid_width - 1)
+    rand_y = random.randint(0, grid_height - 1)
 world_grid.nodes[rand_x][rand_y].items.append("Gator")
 
 for x in world_grid.nodes:
@@ -96,22 +38,19 @@ for x in world_grid.nodes:
             if x % 5 == 0 or y % 4 == 0:
                 world_grid.nodes[x][y].items.append("Wood")
 
-pair_found = False
-
 start_pos = [grid_width * block_size // 2, grid_width * block_size // 2]
 
 player = plr.Player(start_pos)
 
-# Pre game bools
 game_running = True
+has_boat = True
 game_begin = False
 
-# In game bools
-backpack_open = False
-has_boat = False
 while game_running:
     # Screen black
-    screen.fill((0, 0, 255))
+    screen.fill((0, 0, 0, 0))
+    window.fill((0, 0, 255))
+
     # Frame rate
     time_delta = clock.tick(60) / 1000.0
     # Get player position
@@ -120,62 +59,32 @@ while game_running:
 
     gridwise_pos = [(plr_x + screen_size[0] // 2) // block_size, (plr_y + screen_size[1] // 2) // block_size]
     grid_items = world_grid.nodes[gridwise_pos[0]][gridwise_pos[1]].items
-    if len(grid_items) > 0:
-        if grid_items[0] in player.backpack:
-            game_screen_ui.objects["Notification"].text = f"{grid_items[0]} found! Press x to pick up."
-            if not has_boat:
-                game_screen_ui.buttons["Craft"].text = "5 wood to craft a boat"
-        elif grid_items[0] == "Wood" or has_boat:
-            game_screen_ui.objects["Notification"].text = f"??? found? Press x to pick up."
-
-    else:
-        game_screen_ui.objects["Notification"].text = ""
     # Events
-    pack_count = 0
-    for item in player.backpack:
-        pack_count += 1
-        backpack_ui.objects[f"Inventory Cell {pack_count}"].text = item
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_x:
-                if len(grid_items) > 0 and pack_count < 36:
-                    for item in grid_items:
-                        player.backpack.append(item)
-                        grid_items.remove(item)
-
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_pos = pygame.mouse.get_pos()
-            for u in ui_list:
-                for key in u.buttons.keys():
-                    if u.buttons[key].get_bounds().collidepoint(mouse_pos):
-                        if key == "Start Button":
-                            game_begin = True
-                        if key == "Open Backpack":
-                            backpack_open = not backpack_open
-                            if backpack_open:
-                                u.buttons[key].text = "Close Backpack"
-                                u.buttons[key].color = pygame.Color(255, 0, 0)
-                            else:
-                                u.buttons[key].text = "Open Backpack"
-                                u.buttons[key].color = pygame.Color(0, 255, 0)
-                        if key == "Craft" and not has_boat:
-                            wood_count = 0
-                            for item in player.backpack:
-                                if item == "Wood":
-                                    wood_count += 1
-                            if wood_count >= 5:
-                                last_count = wood_count
-                                while wood_count > last_count - 5:
-                                    backpack_ui.objects[f"Inventory Cell {wood_count}"].text = ""
-                                    wood_count -= 1
-                                    player.backpack.remove("Wood")
-                                    has_boat = True
-                                    game_screen_ui.buttons["Craft"].text = "Boat crafted!"
+            for key in gui_manager.buttons:
+                btn = gui_manager.buttons[key]
+                if btn.bounds.collidepoint(mouse_pos):
+                    if key == "start_game_btn":
+                        game_begin = True
+                        btn.enabled = False
+                    if key == "backpack_btn":
+                        inv = gui_manager.objects["backpack_inventory"]
+                        inv.enabled = not inv.enabled
 
     if game_begin:
+        gui_manager.buttons["backpack_btn"].enabled = True
+        gui_manager.objects["alert_box"].enabled = True
+
+        if len(world_grid.nodes[gridwise_pos[0]][gridwise_pos[1]].items) > 0:
+            its = world_grid.nodes[gridwise_pos[0]][gridwise_pos[1]].items
+            gui_manager.objects["alert_box"].text = f"Press x to pick up {its[0]}"
+        else:
+            gui_manager.objects["alert_box"].text = ""
         # Player movement
         keys = pygame.key.get_pressed()
 
@@ -258,18 +167,35 @@ while game_running:
                                              MM_ZOOM))
 
         # Player color change if in water
+                pygame.draw.rect(window, col,
+                                 pygame.Rect(center_x - plr_x, + center_y - plr_y, block_size, block_size))
+
+        for x in range(grid_width):
+            for y in range(grid_height):
+                curr = world_grid.nodes[x][y]
+                col = worldgen.colorMap[curr.biome]
+                if curr.items:
+                    if curr.items[0] == "Wood":
+                        col = 0x00F0F0
+                    else:
+                        col = 0xAB9213
+                if not curr.visited:
+                    col = 0x888888
+                pygame.draw.rect(window, col, pygame.Rect(x * 2 + 10, y * 2 + 10, 2, 2))
+
+        # Player draw
         if not has_boat:
             player_color = 0x880022 if currentBiome == Biome.DEEP_OCEAN else 0xFF0000
         else:
             player_color = 0x964B00 if currentBiome == Biome.DEEP_OCEAN else 0xFF0000
 
         # Actual player
-        pygame.draw.rect(screen, player_color,
+        pygame.draw.rect(window, player_color,
                          pygame.Rect(screen_center[0] - block_size / 4, screen_center[1] - block_size / 4,
                                      block_size / 2, block_size / 2))
 
         # Minimap player
-        pygame.draw.rect(screen, player_color,
+        pygame.draw.rect(window, player_color,
                          pygame.Rect((VISIBLE_BLOCKS_VERT // 2) * MM_ZOOM + MM_OFFSET_X,
                                      (VISIBLE_BLOCKS_VERT // 2) * MM_ZOOM + MM_OFFSET_Y, 2, 2))
 
@@ -277,14 +203,12 @@ while game_running:
         # pygame.draw.rect(screen, 0xAAAAAA,
         #                  pygame.Rect()
 
-    # GUI
-    if game_begin:
-        game_screen_ui.draw_all(screen)
-    else:
-        main_screen_ui.draw_all(screen)
-    if backpack_open:
-        backpack_ui.draw_all(screen)
-    # Draw screen
+    # Draw GUI
+    gui_manager.draw_all(screen)
+    window.blit(screen, (0, 0))
+
+
+    # Update
     pygame.display.update()
 
 pygame.quit()
